51d135b367fddda00d0f3ff5c869b0965efbd76d
jamesespericueta <espericueta2303@gmail.com>
2021-01-09 16:13:11 -0500

    adding teleop functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOpMode.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.roadrunner.drive.MecanumDrive;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -15,17 +14,25 @@ public class MecanumOpMode extends LinearOpMode {
     DcMotor transfer;
     Servo hopperServo;
 
+    Servo elbow;
+    CRServo jaw;
+
     DcMotor flMotor;
     DcMotor frMotor;
     DcMotor blMotor;
     DcMotor brMotor;
     CRServo hopperio;
 
+    /*
+    elbow is 2 and jaw is 3 on hub 3
+     */
 
-    protected MecanumDrive mecanum;
     @Override
     public void runOpMode() throws InterruptedException
     {
+        elbow = hardwareMap.servo.get("elbow");
+        jaw = hardwareMap.crservo.get("jaw");
+
         intake = hardwareMap.dcMotor.get("intake");
         shooter1 = hardwareMap.dcMotor.get("shooter1");
         shooter2 = hardwareMap.dcMotor.get("shooter2");
@@ -42,14 +49,29 @@ public class MecanumOpMode extends LinearOpMode {
         double hopperInputAngle = .1;
         double hopperOutputAngle = .29;
         double hopperAngle = hopperInputAngle;
-
+        double elbowAngle = 0;
+        double jawPower;
         waitForStart();
         while(opModeIsActive())
         {
-            double intakePower = -gamepad1.right_trigger;
-            double shooterPower = -gamepad1.left_trigger;
-            double hopperPower = gamepad1.right_bumper ? 1.0 : 0.0;
-            double transferPower = gamepad1.left_bumper ? 1.0 : 0;
+            jawPower = 0;
+            double intakePower      = -gamepad1.right_trigger;
+            double shooterPower     = -gamepad1.left_trigger;
+            double hopperPower      = 0;//gamepad1.right_bumper ? 1:0;
+            double transferPower    = gamepad1.right_trigger;
+
+            if(gamepad2.right_trigger > gamepad1.right_trigger)
+            {
+                transferPower = -gamepad2.right_trigger;
+                intakePower = gamepad2.right_trigger;
+            }
+            if (gamepad1.right_trigger < .1)
+            {
+                hopperPower = gamepad1.right_bumper ? 1 : 0;
+            }
+            else {
+                hopperPower = -gamepad1.right_trigger/2;
+            }
 
             flMotor.setDirection(DcMotor.Direction.REVERSE);
             blMotor.setDirection(DcMotor.Direction.REVERSE);
@@ -59,8 +81,14 @@ public class MecanumOpMode extends LinearOpMode {
             double twist = -gamepad1.right_stick_x;
 
 
-            if (gamepad1.square) hopperAngle = hopperInputAngle;
-            else if (gamepad1.triangle) hopperAngle= hopperOutputAngle;
+            if (gamepad1.x) hopperAngle = hopperInputAngle;
+            else if (gamepad1.y) hopperAngle= hopperOutputAngle;
+
+            if ( gamepad2.a)  elbowAngle = 0.3;
+            else if ( gamepad2.b) elbowAngle = 0.65;
+
+            if (gamepad2.x) jawPower = .5;
+            else if ( gamepad2.y) jawPower = -.5;
 
 
             double[] speeds = {
@@ -71,8 +99,8 @@ public class MecanumOpMode extends LinearOpMode {
             };
 
             double max = Math.abs(speeds[0]);
-            for( int i = 0; i < speeds.length; i++){
-                if ( max < Math.abs(speeds[i])) max = Math.abs(speeds[i]);
+            for (double speed : speeds) {
+                if (max < Math.abs(speed)) max = Math.abs(speed);
             }
 
             if (max > 1 )
@@ -95,7 +123,10 @@ public class MecanumOpMode extends LinearOpMode {
             transfer.setPower(transferPower);
 
             hopperServo.setPosition(hopperAngle);
-
+            elbow.setPosition(elbowAngle);
+            jaw.setPower(jawPower);
+            telemetry.addData("pressing x: ", "value: " + gamepad2.x);
+            telemetry.addData( "pressing y", "value: " + gamepad2.y);
             telemetry.addData("Motors", "Y Power " + gamepad1.left_stick_y);
             telemetry.addData( "Motors", "Y Power " + gamepad1.left_stick_x);
             telemetry.update();
